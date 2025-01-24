12b3df2440f40c5437143a5ad8bda5b2bb7de1e4
Embla Andersson <emblaandersson@yahoo.se>
2025-01-24 11:33:01 +0100

    Added prompt and answer

--- pages/embla.js
+++ pages/embla.js
@@ -1,3 +1,28 @@
 import { model } from "@/util/ai";
+import { useState } from "react";
 
-export default function Recipes() {}
+export default function Recipes() {
+  const [prompt, setPrompt] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  async function sendPrompt() {
+    const result = await model.generateContent(prompt);
+
+    setAnswer(result.response.text());
+  }
+
+  return (
+    <div>
+      <h2>Recipes suggestions</h2>
+      <input
+        className="border"
+        type="text"
+        onChange={(e) => {
+          setPrompt(e.target.value);
+        }}
+      />
+      <button onClick={sendPrompt}>Send</button>
+      <p>{answer}</p>
+    </div>
+  );
+}
