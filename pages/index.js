545116ddeb32ab623121eaff1189c77f4086cb0c
mmmmm <mustaf.abdulahi@chasacademy.se>
2025-01-24 12:51:15 +0100

    hje

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-<<<<<<< HEAD
       <div className="navbar bg-base-100 top-0 sticky z-50">
         <div className="flex-1 text-xl font-semibold">Home</div>
         <ul className="gap-2 hidden md:flex">
@@ -40,8 +39,6 @@ export default function Home() {
         </button>
       </div>
 
-=======
->>>>>>> 12b3df2440f40c5437143a5ad8bda5b2bb7de1e4
       <div
         className="hero min-h-[450px] bg-center"
         style={{
@@ -139,7 +136,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-<<<<<<< HEAD
       <footer className="footer bg-neutral text-neutral-content p-10">
         <aside>
           <svg
@@ -193,8 +189,6 @@ export default function Home() {
           </div>
         </nav>
       </footer>
-=======
->>>>>>> 12b3df2440f40c5437143a5ad8bda5b2bb7de1e4
     </div>
   );
 }
--- pages/mustaf.js
+++ pages/mustaf.js
@@ -1,1 +1,29 @@
 import { model } from "@/util/ai";
+
+export default function Books() {}
+
+import { useState } from "react";
+
+const BookSuggestions = () => {
+  const [prompt, setPrompt] = useState("");
+  const [answer, setAnswer] = useState("");
+
+  const sendPrompt = async () => {
+    const { response } = await model.generateContent(prompt);
+    setAnswer(response.text());
+  };
+
+  return (
+    <div>
+      <h2>Book Suggestions</h2>
+      <input
+        className="border"
+        type="text"
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
+      <button onClick={sendPrompt}>Send</button>
+      <p>{answer}</p>
+    </div>
+  );
+};
