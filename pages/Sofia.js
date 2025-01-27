f97a45657b5c8403762cf9e340e094a2794c01f3
Sofia Backeholtgit <sbackeholt@gmail.com>
2025-01-27 19:04:35 +0100

    fixed background

--- pages/Sofia.js
+++ pages/Sofia.js
@@ -15,8 +15,24 @@ export default function Music() {
     setAnswer(answerText);
   }
 
+  // <div
+  //     className="grid bg-cover bg-center"
+  //     style={{
+  //       backgroundImage:
+  //         "url(https://img.freepik.com/free-photo/simple-smooth-fabric-textured-background_53876-108254.jpg?ga=GA1.1.1667245943.1733688417)",
+  //     }}>
+  //     <h2 className="flex justify-center text-xl pt-4 md:text-2xl font-semibold ">
+  //       30-minute recipe suggestions
+  //     </h2>
+
   return (
-    <div className="h-svh">
+    <div
+      className="grid bg-cover bg-center"
+      style={{
+        backgroundImage:
+          "url(https://img.freepik.com/free-photo/simple-smooth-fabric-textured-background_53876-108254.jpg?ga=GA1.1.1667245943.1733688417)",
+      }}
+    >
       <div className="text-center m-10">
         <h1 className="text-4xl ">AI Music Recommendations</h1>
         <h2 className="text-2xl">
@@ -30,7 +46,17 @@ export default function Music() {
       </div>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {["Pop", "Jazz", "Rock", "Hip-Hop", "Classical"].map((genre) => (
+        {[
+          "Pop",
+          "Jazz",
+          "Rock",
+          "Hip-Hop",
+          "R&B",
+          "Metal",
+          "K-pop",
+          "Techno",
+          "Classical",
+        ].map((genre) => (
           <div
             key={genre}
             className="card shadow-xl bg-slate-900 hover:bg-slate-950"
