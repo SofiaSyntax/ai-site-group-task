84fe05af9567f2eeead9eadd250881b47dcfb751
Embla Andersson <emblaandersson@yahoo.se>
2025-01-27 17:36:21 +0100

    json removed

--- pages/embla.js
+++ pages/embla.js
@@ -2,16 +2,15 @@ import { model } from "@/util/ai";
 import { useEffect, useState } from "react";
 
 const startPrompt = "30 minute recipe suggestion cooking with:";
-const endPrompt =
-  " give me a json for the name of the meal, ingredient list, and step-by-steps ";
+// const endPrompt =
+//   " give me a json for the name of the meal, ingredient list, and step-by-steps ";
 
 export default function Recipes() {
-  // const [prompt, setPrompt] = useState("");
   const [answer, setAnswer] = useState("");
   const [history, setHistory] = useState([]);
 
   async function sendPrompt(food) {
-    const prompt = startPrompt + food + endPrompt;
+    const prompt = startPrompt + food;
     const result = await model.generateContent(prompt);
     const answerText = result.response.text();
     setAnswer(answerText);
@@ -19,28 +18,10 @@ export default function Recipes() {
     const jsonAnswer = answerText;
     console.log(jsonAnswer);
 
-    // try {
-    //   const parsedAnswer = JSON.parse(answerText);
-    //   console.log(parsedAnswer);
-    //   setAnswer(parsedAnswer);
-    // } catch (error) {
-    //   console.error("Error parsing json:", error);
-    //   setAnswer(null);
-    // }
-
     const newHistory = [...history];
     newHistory.push({ prompt, answer: answerText });
     setHistory(newHistory);
   }
-
-  // async function sendOnPageLoad(question) {
-  //   const result = await model.generateContent(question);
-  //   setAnswer(result.response.text());
-  // }
-
-  // useEffect(() => {
-  //   sendOnPageLoad("give me a 30 minute recipe suggestion cooking with:");
-  // }, []);
 
   useEffect(() => {
     if (history.length > 0) {
@@ -53,12 +34,11 @@ export default function Recipes() {
       className="grid bg-cover bg-center"
       style={{
         backgroundImage:
-          "url(https://img.freepik.com/free-vector/purple-color-background-gradient-modern-design-abstract_343694-2146.jpg?t=st=1737985733~exp=1737989333~hmac=e69f8013ddf485d7546c4d50de652cd3f3c076872ed4dc55412b646574f0b363&w=740)",
+          "url(https://img.freepik.com/free-photo/simple-smooth-fabric-textured-background_53876-108254.jpg?ga=GA1.1.1667245943.1733688417)",
       }}>
       <h2 className="flex justify-center text-xl pt-4 md:text-2xl font-semibold ">
         30-minute recipe suggestions
       </h2>
-
       <div className="md:grid md:grid-cols-3 md:justify-center md:gap-14 md:m-6 flex flex-wrap gap-8 m-4 pt-2 justify-center ">
         {[
           "Chicken",
@@ -75,15 +55,16 @@ export default function Recipes() {
             <div className="card shadow-xl bg-slate-900 hover:bg-slate-950">
               <div className="card-body">
                 <h2 className="card-title">{food}</h2>
-                <h3>Get {food} recommendations</h3>
+                <h3>Get {food} recipes</h3>
                 <div className="card-actions justify-end">
-                  <button className="btn btn-primary">recommend</button>
+                  <button className="btn btn-primary">Give me a recipe</button>
                 </div>
               </div>
             </div>
           </div>
         ))}
       </div>
+      <h3 className=" text-2xl font-semibold px-6">30 minute recipe:</h3>
       <p>{answer}</p>
     </div>
   );
